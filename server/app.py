1d4d5b86749e8c795d7b02ebd73d3e4e66c8ce08
BookmDan <danoh329@gmail.com>
2023-12-27 16:49:12 -0800

    partial pass sqlite3.IntegrityErrror

--- server/app.py
+++ server/app.py
@@ -17,11 +17,85 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list = []
+
+        for plant in plants:
+            plant_data = {
+                'id': plant.id,
+                'name': plant.name,
+                'image': plant.image,
+                'price': float(plant.price)
+            }
+            plant_list.append(plant_data)
+
+        response = make_response(
+            plant_list,
+            200,
+        )
+        return response
+        # return jsonify(plant_list)
+    
+    def post(self):
+        data = request.get_json()
+        if 'name' not in data or 'image' not in data or 'price' not in data:
+            response = make_response(
+                {"message": "Please provide values for 'name', 'image', and 'price'."},
+                400,
+            )
+            return response
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        # db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            response = make_response(
+                {"message": f"Failed to create the plant. Error: {str(e)}"},
+                500,
+            )
+
+            return response
+        response = make_response(
+            new_plant.to_dict(),
+            201,
+        )
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self,id):
+        plant= Plant.query.get(id)
+        if plant:
+            plant_data = {
+                'id':plant.id,
+                'name':plant.name,
+                'image':plant.image,
+                'price': float(plant.price)
+            }
+
+            response = make_response(
+                plant_data,
+                200,
+            )
+        else: 
+            response = make_response(
+                {"message": f"Plant {id} not found."},
+                404,
+            )
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,10 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=True)  
+    price = db.Column(db.DECIMAL(precision=10, scale=2), nullable=False)
+
+
+    def __repr__(self):
+        return f'<Plant {self.id}, {self.name}, {self.price}>'
